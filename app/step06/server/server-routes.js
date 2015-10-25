dc43e6744701b76072c800b6938b0ef744488b80
dcsan <dc@pikkle.com>
2015-10-25 21:26:30 +0800

    todo notes

--- app/step06/server/server-routes.js
+++ app/step06/server/server-routes.js
@@ -6,6 +6,7 @@ Picker.route('/api/:username', function(params, req, res, next) {
 		};
 		// res.end(post.content);
 		// res.send(post);
+	// Chatlogs.insert();
 	res.end(JSON.stringify(blob, {indent: true}) );
 
 });
