feb98161961c463d7a46721cfad2a910c2e5d7be
dcsan <dc@pikkle.com>
2015-11-04 21:59:14 +0800

    reactive vars in stop10

--- app/step10/lib/router.jsx
+++ app/step10/lib/router.jsx
@@ -1,47 +1,54 @@
 FlowRouter.route('/', {
-	action: function(params, queryParams) {
-		console.log("route> top", params);
-		ReactLayout.render(TopComponent)
-	}
+  action: function(params, queryParams) {
+    console.log("route> top", params);
+    ReactLayout.render(TopComponent)
+  }
 });
 
 FlowRouter.route('/material', {
-	action: function(params, queryParams) {
-		console.log("route> top", params);
-		ReactLayout.render(MaterialApp)
-	}
+  action: function(params, queryParams) {
+    console.log("route> top", params);
+    ReactLayout.render(MaterialApp)
+  }
 });
 
 
 FlowRouter.route('/home/:username', {
-	action: function(params, queryParams) {
-		console.log("route> home", params);
-		ReactLayout.render(
-			MainLayout, {
-				content: <HomeComponent username={params.username} />
-			}
-		)
-	}
+  action: function(params, queryParams) {
+    console.log("route> home", params);
+    ReactLayout.render(
+      MainLayout, {
+        content: <HomeComponent username={params.username} />
+      }
+    )
+  }
 });
 
 
 FlowRouter.route('/players', {
-	action: function(params, queryParams) {
-		ReactLayout.render(
-			MainLayout, {
-				content: <PlayerListComponent/>
-			}
-		)
-	}
+  action: function(params, queryParams) {
+    ReactLayout.render(
+      MainLayout, {
+        content: <PlayerListComponent/>
+      }
+    )
+  }
 });
 
 
 FlowRouter.route('/chatlogs/:username', {
-	action: function(params, queryParams) {
-		ReactLayout.render(
-			MainLayout, {
-				content: <ChatlogsComponent/>
-			}
-		)
-	}
+  action: function(params, queryParams) {
+    ReactLayout.render(
+      MainLayout, {
+        content: <ChatlogsComponent/>
+      }
+    )
+  }
+});
+
+
+FlowRouter.route('/var', {
+  action: function(params, queryParams) {
+    ReactLayout.render(VarComponent)
+  }
 });
